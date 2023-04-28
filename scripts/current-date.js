8a67025b943107b89d677b1c7daacf4988c00f30
aribyui <ari22008@byui.edu>
2023-04-28 12:55:16 -0600

    current-date fixed

--- scripts/current-date.js
+++ scripts/current-date.js
@@ -1,32 +1,10 @@
-const daynames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+const d = new Date();
+const day = d.getDate();
+const month = d.getMonth() + 1;
+const year = d.getFullYear();
+const hour = d.getHours();
+const minutes = d.getMinutes();
+const seconds = d.getSeconds();
 
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-const d1 = new Date();
-const dayname = daynames[d1.getDay()];
-const monthname = months[d1.getMonth()];
-const year = d1.getFullYear();
 document.querySelector("#year").textContent = year;
-
-let lastmodified = new Date(document.lastModified);
-document.querySelector("#last-modified").textContent = lastmodified;
+document.querySelector("#date-hour").textContent = `${month}/${day}/${year} ${hour}:${minutes}:${seconds}`;
