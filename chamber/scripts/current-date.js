43c913ed7f481417d35e92bce67963bf62ba6d10
aribyui <ari22008@byui.edu>
2023-05-14 20:09:22 -0600

    current-date.js added

--- chamber/scripts/current-date.js
+++ chamber/scripts/current-date.js
@@ -1,11 +1,39 @@
+const daynames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+
 const d = new Date();
 const day = d.getDate();
 const month = d.getMonth() + 1;
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
 const year = d.getFullYear();
 const hour = d.getHours();
-const minute = d.getMinutes();
+const minutes = d.getMinutes();
 const seconds = d.getSeconds();
+const fulldate = `${dayName}, ${day} ${monthName} ${year}`;
 
-
-
-document.querySelector(".base-footer").innerHTML = `<p>&copy;${year} Nuevo León Chamber | Lizandro Arizpe | WDD 230 Project | Last Modification: ${day}/${month}/${year} ${hour}:${minute}:${seconds}</p>`;
+document.querySelector(".current-date").textContent = fulldate;
+document.querySelector("#year").textContent = year;
+document.querySelector("#last-modification").textContent = `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`;
